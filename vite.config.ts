f73858a906b6a08636fcf8a82926ada515cc8199
amertomysense-lang <amertomysense@gmail.com>
2026-06-12 01:25:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,3 @@
-import { defineConfig } from "vite";
-import { defineConfig as defineLovableConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-// نقوم بدمج إعدادات لوفابل الافتراضية مع توجيه السيرفر لـ Cloudflare
-export default defineLovableConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-    nitro: {
-      preset: "cloudflare-pages"
-    },
-  },
-  build: {
-    outDir: ".output/public"
-  }
-});
+export default defineConfig({});
