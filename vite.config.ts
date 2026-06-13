f512d6ea74647d3582dbf0ab030375b7265e8ea5
amertomysense-lang <amertomysense@gmail.com>
2026-06-14 00:44:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { defineConfig as defineLovableConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+export default defineLovableConfig({
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+  build: {
+    outDir: ".output/public"
+  }
+});
